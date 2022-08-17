764e7ede0115db61a5fe2721b9c57079e1a19d37
Victor <ovchinnv@georgetown.edu>
2022-08-17 19:21:29 -0400

    local commit before pulls

--- matlab/anal/ntaa.m
+++ matlab/anal/ntaa.m
@@ -1,6 +1,6 @@
 % nucleotide and amino acid code conversion functions
 
-aas={ 'ALA' 'ARG' 'ASP' 'GLN' 'LEU' 'THR' 'GLU' 'ILE' 'PHE' 'LYS' 'SER' 'VAL' 'MET' 'ASN' 'PRO' 'TYR' 'HIS' 'HSD' 'HSE' 'GLY' 'TRP' 'CYS' ;
+aas={ 'ALA' 'ARG' 'ASP' 'GLN' 'LEU' 'THR' 'GLU' 'ILE' 'PHE' 'LYS' 'SER' 'VAL' 'MET' 'ASN' 'PRO' 'TYR' 'HSD' 'HIS' 'HSE' 'GLY' 'TRP' 'CYS' ;
       'A'   'R'   'D'   'Q'   'L'   'T'   'E'   'I'   'F'   'K'   'S'   'V'   'M'   'N'   'P'   'Y'   'H'   'H'   'H'   'G'   'W'   'C' } ;
 aa1= @(x) char(aas(find(ismember(aas(:), x),1)+1)); % get 1 letter code from 3 letter code
 aa3= @(x) char(aas(find(ismember(aas(:), x),1)-1)); % get 3 letter code from 1 letter code
