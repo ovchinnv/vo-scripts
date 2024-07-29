9328bd14ddd12a3cf75056dcf14453c611a5b678
Victor <ovchinnv@georgetown.edu>
2024-07-29 11:45:16 -0400

    mods to chomm.py

--- openmm/CHOMM.py
+++ openmm/CHOMM.py
@@ -617,6 +617,10 @@ if 1:
  fxsc.close();
 #==== reset switching distance
 # del switchdist;
+ if (conscor==None):
+  del conscor;
+ if (conspdb==None):
+  del conspdb;
  if (dynamo): # dynamo is somewhat problematic upon run continuation, need a complete reinit because the end of each run destroys the dynamo object
   del system;
   del simulation;
